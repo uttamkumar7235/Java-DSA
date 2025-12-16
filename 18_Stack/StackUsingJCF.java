cb05513099cf6661f09a70ae95f847676a309596
Uttam Kumar <uttamkumar7235@gmail.com>
2025-12-16 17:53:02 +0530

    Stack using Java Collection Framework

---
+++ 18_Stack/StackUsingJCF.java
@@ -0,0 +1,19 @@
+// Stack Using Java Collection Framework
+
+import java.util.*;
+
+public class StackUsingJCF {
+
+    public static void main(String[] args) {
+        
+        Stack<Integer> s = new Stack<>();
+
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.pop());
+        }
+    }
+}
