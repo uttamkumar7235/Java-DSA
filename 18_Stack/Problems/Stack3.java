8109802e568cfaa87aefba35f7ee03a4dd559fab
Uttam Kumar <uttamkumar7235@gmail.com>
2025-12-17 19:32:17 +0530

    Reverse a Stack

---
+++ 18_Stack/Problems/Stack3.java
@@ -0,0 +1,44 @@
+// Reverse a Stack
+
+import java.util.Stack;
+
+public class Stack3 {
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    
+    public static void reverseStack(Stack<Integer> s) {
+
+        if (s.isEmpty()) {
+            return;
+        }
+
+        int top = s.pop();
+        reverseStack(s);
+        pushAtBottom(s, top);
+        
+    }
+
+    public static void main(String[] args) {
+        
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        s.push(4);
+
+        System.out.println(s);
+        
+        reverseStack(s);
+        System.out.println(s);
+    }
+}
