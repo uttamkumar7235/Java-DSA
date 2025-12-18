4a147bfedaef8b96661a30b1f33c0921adb22140
Uttam Kumar <uttamkumar7235@gmail.com>
2025-12-18 21:05:59 +0530

    Bubble Sort

---
+++ 08_Basic_Sorting_Algorithms/BubbleSort.java
@@ -0,0 +1,39 @@
+// Bubble Sort
+
+public class BubbleSort {
+
+    public static void bubbleSort(int arr[]) {
+
+        for (int i = 0; i < arr.length-1; i++) {
+            for (int j = 0; j < arr.length-1-i; j++){
+
+                if (arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public static void printArray (int arr[]) {
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        int arr[] = {5, 4, 1, 3, 2};
+
+        System.out.print("Before Sorting : ");
+        printArray(arr);
+
+        bubbleSort(arr);
+        System.out.print("After Sorting : ");
+        printArray(arr);
+
+    }
+}
