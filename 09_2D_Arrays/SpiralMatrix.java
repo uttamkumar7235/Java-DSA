48af839b1a24aa96288d079b34642688fa0b8a29
Uttam Kumar <uttamkumar7235@gmail.com>
2025-12-22 19:46:47 +0530

    Added Print Spiral Matrix

---
+++ 09_2D_Arrays/SpiralMatrix.java
@@ -0,0 +1,68 @@
+public class SpiralMatrix {
+
+    public static void printSpiral(int matrix[][]) {
+        int startRow = 0;
+        int startCol = 0;
+        int endRow = matrix.length-1;
+        int endCol = matrix[0].length-1;
+
+        System.out.print("Spiral Matrix : ");
+
+        while (startRow <= endRow && startCol <= endCol) {
+
+            // Top
+            for (int j = startCol; j <= endCol; j++) {
+                System.out.print(matrix[startRow][j] + " ");
+            }
+
+            // Right
+            for (int i = startRow+1; i <= endRow; i++) {
+                System.out.print(matrix[i][endCol] + " ");
+            }
+
+            // Bottom
+            for (int j = endCol-1; j >= startCol; j--) {
+                if (startRow == endRow) {
+                    break;
+                }
+                System.out.print(matrix[endRow][j] + " ");
+            }
+
+            // Left
+            for (int i = endRow-1; i >= startRow+1; i--) {
+                if (startCol == endCol) {
+                    break;
+                }
+                System.out.print(matrix[i][startCol] + " ");
+            }
+            startCol++;
+            startRow++;
+            endCol--;
+            endRow--;
+        }
+    }
+
+    public static void printMatrix(int matrix[][]) {
+
+        for (int i=0; i<matrix.length; i++) {
+            for (int j=0; j<matrix[0].length; j++) {
+                System.out.print(matrix[i][j] + "  ");
+            }
+            System.out.println();
+        }
+        
+    }
+
+    public static void main(String args[]) {
+
+        int matrix[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        System.out.println("Matrix : ");
+        printMatrix(matrix);
+        printSpiral(matrix);
+    }
+}
