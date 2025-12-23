6bf7d18847b4d16011fbef5d381b886a346c4eca
Uttam Kumar <uttamkumar7235@gmail.com>
2025-12-23 18:48:31 +0530

    Find Largest String (Lexicographically)

---
+++ 10_Strings/LargestString.java
@@ -0,0 +1,17 @@
+// Largest String (Lexicographical comparision)
+
+public class LargestString {
+
+    public static void main(String[] args) {
+
+        String fruits[] = {"apple","mango","banana"};
+        String largest = fruits[0];
+
+        for (int i = 1; i < fruits.length; i++) {
+            if (largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+}
