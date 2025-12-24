cae6319f3224382a31ea621292fd225051c2f518
Uttam Kumar <uttamkumar7235@gmail.com>
2025-12-24 18:22:20 +0530

    Check Power of 2

---
+++ 11_Bit_Manipulation/PowerOfTwo.java
@@ -0,0 +1,12 @@
+public class PowerOfTwo {
+    
+    public static boolean isPowerOf2(int n) {
+        return (n & (n-1)) == 0;
+    }
+
+    public static void main(String[] args) {
+        
+        System.out.println(isPowerOf2(8));
+        System.out.println(isPowerOf2(15));
+    }
+}
